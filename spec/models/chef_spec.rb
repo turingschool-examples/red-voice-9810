de88b5968c69242b0b6d5a6177d8c4050592448f
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-02-26 18:48:38 -0500

    Refactor: edit test for instance methods

--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -14,6 +14,8 @@ RSpec.describe Chef, type: :model do
 
     @tacos = @chef_laura.dishes.create!(name: "Tacos", description: "For Tuesdays")
     @enchiladas = @chef_laura.dishes.create!(name: "Enchilidas", description: "Friday Special")
+    @burrito = @chef_laura.dishes.create!(name: "Burrito", description: "Monday Only!")
+    @carnitas_plate = @chef_laura.dishes.create!(name: "Carnitas Plate", description: "Weekends")
 
     @tortilla = Ingredient.create!(name: 'Tortilla', calories: 150)
     @cheese = Ingredient.create!(name: 'Cheese', calories: 200)
@@ -29,13 +31,21 @@ RSpec.describe Chef, type: :model do
     @dish_ing_5 = DishIngredient.create!(dish_id: @enchiladas.id, ingredient_id: @cheese.id )
     @dish_ing_6 = DishIngredient.create!(dish_id: @enchiladas.id, ingredient_id: @sauce.id )
     @dish_ing_6 = DishIngredient.create!(dish_id: @enchiladas.id, ingredient_id: @carnitas.id )
+    @dish_ing_4 = DishIngredient.create!(dish_id: @burrito.id, ingredient_id: @tortilla.id )
+    @dish_ing_6 = DishIngredient.create!(dish_id: @burrito.id, ingredient_id: @carnitas.id )
+    @dish_ing_6 = DishIngredient.create!(dish_id: @carnitas_plate.id, ingredient_id: @carnitas.id )
   end
 
   describe '.instance methods' do
     describe '.ingredient_list' do
       it 'gets all ingredients a chef uses' do
-        results = [@carnitas.name, @cheese.name, @sauce.name, @lettuce.name ,@tortilla.name,]
-        expect(@chef_laura.ingredient_list).to eq(results)
+
+        results = @chef_laura.ingredient_list
+        expect(results).to include(@carnitas.name)
+        expect(results).to include(@cheese.name)
+        expect(results).to include(@sauce.name)
+        expect(results).to include(@lettuce.name)
+        expect(results).to include(@tortilla.name)
       end
     end
 
@@ -44,8 +54,8 @@ RSpec.describe Chef, type: :model do
 
         results = @chef_laura.most_popular_ingredients
         expect(results[0].name).to eq(@carnitas.name)
-        expect(results[1].name).to eq(@cheese.name)
-        expect(results[2].name).to eq(@tortilla.name)
+        expect(results[1].name).to eq(@tortilla.name)
+        expect(results[2].name).to eq(@cheese.name)
         expect(results.length).to eq(3)
       end
     end
