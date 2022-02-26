379c5ad86eee4fe19dc57fc0b94f84f4678b80ee
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-02-26 18:24:10 -0500

    Feat: add instance method most popilar ingredients

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -7,4 +7,11 @@ class Chef <ApplicationRecord
       .select("ingredients.name").distinct
       .pluck('ingredients.name')
   end
+
+  def most_popular_ingredients
+    dishes.joins(:ingredients, :dish_ingredients)
+      .select("ingredients.name, COUNT(ingredients.*) AS total_ingredient_count")
+      .group('ingredients.name').order('total_ingredient_count DESC')
+      .limit(3)
+  end
 end
