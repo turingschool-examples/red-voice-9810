a087fd6419a90b5495cfd2d301a8fb56c34853fa
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-02-26 18:47:59 -0500

    Feat: add association test for ingredient model

--- spec/models/dish_ingredient_spec.rb
+++ spec/models/dish_ingredient_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe DishIngredient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should belong_to :ingredient}
+  it {should belong_to :dish}
 end
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Ingredient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should have_many :dish_ingredients}
+  it {should have_many(:dishes).through(:dish_ingredients)}
 end
