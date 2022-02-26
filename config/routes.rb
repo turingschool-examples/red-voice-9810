568e086a66c65ddc9be12d9ba0ef2c027dcc315b
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-02-26 17:58:29 -0500

    Feat: limit route access

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :chefs
-  resources :dishes
-  resources :ingredients
+  resources :chefs, only: [:show] do
+    resources :ingredients, controller: 'chef_ingredients', only: [:index]
+  end
+
+  resources :dishes, only: [:show]
 end
