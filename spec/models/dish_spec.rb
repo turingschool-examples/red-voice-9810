62d959387a72725e4b79fe00faa32a04cc6448cf
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-27 10:44:33 -0700

    test: Write test for dish model instance method calorie_count

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,24 @@ RSpec.describe Dish, type: :model do
     it {should have_many :dish_ingredients}
     it { should have_many(:ingredients).through(:dish_ingredients) }
   end
+
+  before :each do 
+    @chef = Chef.create!(name:"Don")
+    @dish1 = Dish.create!(name: "Sticky Rice", description: "tangy and sweet", chef_id: @chef.id)
+    @dish2 = Dish.create!(name: "Mochi", description: "doughy with ice cream core", chef_id: @chef.id)
+    @ingredient1 = Ingredient.create!(name: "Dough", calories: 100)
+    @ingredient2 = Ingredient.create!(name: "Ice Cream", calories: 125)
+    @ingredient3 = Ingredient.create!(name: "Rice", calories: 40)
+
+    @joins_row1 = DishIngredient.create!(ingredient_id: @ingredient1.id, dish_id: @dish2.id)
+    @joins_row2 = DishIngredient.create!(ingredient_id: @ingredient2.id, dish_id: @dish2.id)
+  end
+
+  describe 'instance methods' do 
+    describe '#calorie_count' do 
+      it 'returns the sum of calories for all ingredients that belong to a dish' do 
+        expect(@dish2.calorie_count).to eq(225)
+      end
+    end
+  end 
 end
