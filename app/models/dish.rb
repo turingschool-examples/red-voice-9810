8f9addb7f31e3ec11c981d9d8211db72f7cf8b71
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-27 10:44:50 -0700

    define method calorie_count, pass test

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -3,4 +3,10 @@ class Dish <ApplicationRecord
   belongs_to :chef
   has_many :dish_ingredients
   has_many :ingredients, through: :dish_ingredients
+
+# I'm still not 100% sure whether it's better to write this method here as an instance method 
+# Or if it would be more restful to put this method in Ingredient as a class method 
+  def calorie_count
+   ingredients.sum(:calories)
+  end 
 end
