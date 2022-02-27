47e9a3d9d4441ff3c8213a47ae1569ea7e7da0e7
Austin Moore <austinchristianmoore@gmail.com>
2022-02-27 16:41:36 -0500

    Feature: Write US2 tests

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -1,11 +1,5 @@
 require 'rails_helper'
 
-# As a visitor
-# When I visit a dish's show page
-# I see the dish’s name and description
-# And I see a list of ingredients for that dish
-# And I see the chef's name
-
 RSpec.describe "dish show page", type: :feature do
   it "visits the show page" do
     chef_1 = Chef.create!(name: "Austin")
@@ -61,4 +55,30 @@ RSpec.describe "dish show page", type: :feature do
     expect(page).to have_content(chef_1.name)
     expect(page).to_not have_content(chef_2.name)
   end
+
+  it "displays the total calorie count of the dishes ingredients" do
+    chef_1 = Chef.create!(name: "Austin")
+    chef_2 = Chef.create!(name: "Noel")
+
+    dish_1 = chef_1.dishes.create!(name: "Cereal", description: "Pour cereal, pour milk")
+    dish_2 = chef_2.dishes.create!(name: "Protein Shake", description: "Pour powder, pour milk")
+
+    ingredient_1 = Ingredient.create!(name: "Fruit Loops", calories: 50)
+    ingredient_2 = Ingredient.create!(name: "Milk", calories: 100)
+    ingredient_3 = Ingredient.create!(name: "Protein Powder", calories: 150)
+
+    dish_ingredient_1 = DishIngredient.create!(dish_id: dish_1.id, ingredient_id: ingredient_1.id)
+    dish_ingredient_2 = DishIngredient.create!(dish_id: dish_1.id, ingredient_id: ingredient_2.id)
+    dish_ingredient_3 = DishIngredient.create!(dish_id: dish_2.id, ingredient_id: ingredient_1.id)
+    dish_ingredient_4 = DishIngredient.create!(dish_id: dish_2.id, ingredient_id: ingredient_3.id)
+
+    visit "/dishes/#{dish_1.id}"
+
+    expect(page).to have_content("Total Calories: 150")
+    expect(page).to_not have_content("Total Calories: 200")
+  end
 end
+
+# As a visitor
+# When I visit a dish's show page
+# I see the total calorie count for that dish.
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,17 @@ RSpec.describe Dish, type: :model do
     it {should have_many(:dish_ingredients)}
     it {should have_many(:ingredients).through(:dish_ingredients)}
   end
+
+  describe 'instance methods' do
+    it "calculates the sum of all a dishes ingredients calories" do
+      chef_1 = Chef.create!(name: "Austin")
+      dish_1 = chef_1.dishes.create!(name: "Cereal", description: "Pour cereal, pour milk")
+      ingredient_1 = Ingredient.create!(name: "Fruit Loops", calories: 50)
+      ingredient_2 = Ingredient.create!(name: "Milk", calories: 100)
+      dish_ingredient_1 = DishIngredient.create!(dish_id: dish_1.id, ingredient_id: ingredient_1.id)
+      dish_ingredient_2 = DishIngredient.create!(dish_id: dish_1.id, ingredient_id: ingredient_2.id)
+
+      expect(dish_1.total_calories).to eq(150)
+    end
+  end
 end
