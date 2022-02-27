2972a85c17a3e306db6a550b26af8d353a523b9a
Aedan Yturralde <aedanyturralde@gmail.com>
2022-02-27 11:13:17 -0700

    refactor routes from namespace to resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :dishes, only: [:show]
   resources :chefs, only: [:show]
+
+  resources :chefs, module: :chef do 
+    resources :ingredients, only: [:index]
+  end 
 end
