66e6185dd0018ae1f9e9b1dd638919c77b80289e
Michael Brandt <msbrandt00@gmail.com>
2022-02-28 04:00:24 -0700

    relationships between tables and adding ingredient to dish

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe Dish, type: :model do
   describe "relationships" do
     it {should belong_to :chef}
   end
+  describe 'instance methods' do 
+    it 'will add an ingredient to a dish' do 
+      chef_1 = Chef.create!(name: 'Tom')
+      ingredient_1 = Ingredient.create!(name: 'Cheese', calories: 30)
+      ingredient_2 = Ingredient.create!(name: 'Linguini', calories: 50)
+      dish_1 = Dish.create!(name: 'Pasta', description: 'Mac and Cheese', chef_id: chef_1.id)
+      dish_1.add_ingredient(ingredient_1)
+      dish_1.add_ingredient(ingredient_2)
+      expect(dish_1.ingredients).to eq([ingredient_1, ingredient_2])
+    end
+  end
 end
