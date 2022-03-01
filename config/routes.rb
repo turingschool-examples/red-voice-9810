aada085f8409746e585ad1f3a5530d55a0831a67
Katy Harrod <m.catherine.harrod@gmail.com>
2022-02-28 17:44:00 -0700

    User story 1 complete for dish show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/chefs/:chef_id/dishes/:dish_id', to: 'dishes#show'
 end
