043733f5563b1b2e5d76471a72aea3f20835dfbd
Becky Nisttahuz <benistta@hotmail.com>
2022-03-01 13:32:53 -0700

    Test: Create test for relationship in model spec

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -7,5 +7,7 @@ RSpec.describe Dish, type: :model do
   end
   describe "relationships" do
     it {should belong_to :chef}
+    it { should have_many :ingredient_dishes }
+    it { should have_many(:ingredients).through :ingredient_dishes }
   end
 end
