cb8ebcc1da96b0300f9f6c7453bb79820d4f17b2
Maximilian Wagner <maw3350@gmail.com>
2022-03-01 16:42:17 -0500

    test: add model test for ingredient(relationships)

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,7 +2,7 @@ class Ingredient <ApplicationRecord
   validates_presence_of :name, :calories
 
 
-  has_many :dishes, though: :dish_ingredients
   has_many :dish_ingredients
+  has_many :dishes, through: :dish_ingredients
 
 end
