bc37c1f58166ec63c0bdf3ef3ec56a43bb94d973
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-01 16:30:23 -0700

    Feat: add nested route to chefs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
-  resources :chefs, only: [:show]
+  resources :chefs, only: [:show] do
+    resources :ingredients, controller: 'chef_ingredients', only: [:index]
+  end
   resources :dishes, only: [:show]
 end
