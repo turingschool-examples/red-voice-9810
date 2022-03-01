df4b4f9b34b52e7c69e5e62b45d9f9347499c34c
Katy Harrod <m.catherine.harrod@gmail.com>
2022-02-28 18:52:08 -0700

    Model testing for uniq ingredients method

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,7 @@ class Chef <ApplicationRecord
   validates_presence_of :name
   has_many :dishes
 
+  def uniq_ingredients
+    dishes.joins(:ingredients).select('ingredients.name').distinct
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get '/chefs/:chef_id/dishes/:dish_id', to: 'dishes#show'
+
+  get '/chefs/:id', to: 'chefs#show'
+
+  get '/chefs/:id/ingredients', to: 'chefs#index'
 end
