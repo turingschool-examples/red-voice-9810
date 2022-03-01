64fc89867d733ac8d2d68c5c066f990ae2b53fc5
Ruben Pasillas <hobbiathan@gmail.com>
2022-03-01 16:00:10 -0700

    add model relations and name prsence validations

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -3,4 +3,12 @@ class Dish <ApplicationRecord
   belongs_to :chef
   has_many :dish_ingredients
   has_many :ingredients, through: :dish_ingredients
+
+  def total_calories
+    total = ingredients.map do |ing|
+      ing.calories
+    end
+
+    total.sum
+  end
 end
