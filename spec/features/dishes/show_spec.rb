91968701e4ca13da21364f3cb98448ebe79d632a
Sam Smith <samlsmith424@gmail.com>
2022-03-01 13:17:55 -0800

    User story 2 complete

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -29,4 +29,20 @@ RSpec.describe 'dish show page' do
     expect(page).to have_content(butter.name)
     expect(page).to have_content(chef_1.name)
   end
+
+  it 'shows the total calorie count for the dish' do
+    chef_1 = Chef.create!(name: "Gordon Ramsey")
+    risotto = chef_1.dishes.create!(name: "Risotto", description: "Italian rice dish")
+    rice = Ingredient.create!(name: "rice", calories: 300)
+    broth = Ingredient.create!(name: "broth", calories: 250)
+    butter = Ingredient.create!(name: "butter", calories: 400)
+
+    DishIngredient.create!(dish_id: risotto.id, ingredient_id: rice.id)
+    DishIngredient.create!(dish_id: risotto.id, ingredient_id: broth.id)
+    DishIngredient.create!(dish_id: risotto.id, ingredient_id: butter.id)
+
+    visit "/dishes/#{risotto.id}"
+
+    expect(risotto.calorie_count).to eq(950)
+  end
 end
