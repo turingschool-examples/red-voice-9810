98f440618a6104c231cf10652973777099a8f8e5
Sam Iavri <sammi80134@gmail.com>
2022-03-01 15:36:39 -0700

    Im not sure why my sum function isnt working... user story 2

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -17,4 +17,16 @@ RSpec.describe 'Show page' do
     expect(page).to have_content(ingredient2.name)
     expect(page).to have_content(ingredient3.name)
   end
+
+  it 'shows the total calorie count for a dish' do
+    chef = Chef.create!(name: 'King Chef')
+    dish1 = chef.dishes.create!(name: 'King Krabé', description: 'Only food a king can eat')
+    ingredient1 = dish1.ingredients.create!(name: 'Crab', calories: 20)
+    ingredient2 = dish1.ingredients.create!(name: 'King Butter', calories: 100)
+    ingredient3 = dish1.ingredients.create!(name: 'Gold', calories: 0)
+
+    visit "/dishes/#{dish1.id}"
+
+    expect(page).to have_content('Total Calorie Count: 120')
+  end
 end
