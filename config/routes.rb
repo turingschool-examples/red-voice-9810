0a3d42038c256caae9be3705706640674ec4b1b1
TJ Williams <williamsjr.timothy1@gmail.com>
2022-03-01 18:53:36 -0500

    Add: Chef and chef ingredients routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :dish, only: [:show]
+  resources :chef, only: [:show]
+
+  get "/chef/:id/ingredients", to: "chef_ingredients#index"
 end
