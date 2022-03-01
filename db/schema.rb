ad86b61e9d594a36957d3758ffcf0e2aaa9f330f
Ruben Pasillas <hobbiathan@gmail.com>
2022-03-01 15:41:30 -0700

    Add Calories collumn to ingredients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_01_221043) do
+ActiveRecord::Schema.define(version: 2022_03_01_224057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2022_03_01_221043) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "calories"
   end
 
   add_foreign_key "dish_ingredients", "dishes"
