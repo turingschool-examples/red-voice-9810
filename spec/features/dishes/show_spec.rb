cb220b70af6dad0ba48af4090767e10d5d77ef39
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-03-01 14:05:19 -0700

    Initital commit

---
+++ spec/features/dishes/show_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+# As a visitor
+# When I visit a dish's show page
+# I see the dish’s name and description
+# And I see a list of ingredients for that dish
+# And I see the chef's name
