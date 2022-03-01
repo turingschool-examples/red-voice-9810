92a282c724f122ed500c08fe1742598bede3a7d5
Becky Nisttahuz <benistta@hotmail.com>
2022-03-01 13:36:30 -0700

    Feat: model relationship tests pass

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,4 +1,6 @@
 class Dish <ApplicationRecord
   validates_presence_of :name, :description
   belongs_to :chef
+  has_many :ingredient_dishes
+  has_many :ingredients, through: :ingredient_dishes
 end
