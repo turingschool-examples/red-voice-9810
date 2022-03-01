0b56e4c951e5c96a9852b52277d99f15e81214e7
Conor Barthel <conor.barthel@gmail.com>
2022-03-01 12:55:05 -0700

    Feat:Test and functionalliy for user story 1

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -3,4 +3,8 @@ class Dish < ApplicationRecord
   belongs_to :chef
   has_many :dish_ingredients
   has_many :ingredients, through: :dish_ingredients
+
+  def chef_name
+    Chef.find(chef_id).name
+  end
 end
