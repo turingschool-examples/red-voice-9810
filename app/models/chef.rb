32c8345c860e62a139ceafa7e363745cb2ad2caf
Katy Harrod <m.catherine.harrod@gmail.com>
2022-02-28 19:52:07 -0700

    Final commit -- all required user stories complete

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -5,4 +5,20 @@ class Chef <ApplicationRecord
   def uniq_ingredients
     dishes.joins(:ingredients).select('ingredients.name').distinct
   end
+
+
+  def top_3
+    binding.pry
+    self.dishes.joins(:ingredients)
+    .select('ingredients.name')
+    .group('ingredients.name').count
+  end
+
+  # in my view if I would have gotten the method down
+
+  # <div id="top-3">
+  #   <% @chef.top_3.each do |ingredient| %>
+  #     <%= ingredient.name %>
+  #   <% end %>
+  # </div>
 end
