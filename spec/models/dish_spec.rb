b5f7dbbe5781d3518a1b43670dfddaf2afe218dd
Maximilian Wagner <maw3350@gmail.com>
2022-03-01 17:56:50 -0500

    test: add #calorie_count model test

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -8,4 +8,22 @@ RSpec.describe Dish, type: :model do
   describe "relationships" do
     it {should belong_to :chef}
   end
+
+  describe "instance methods" do
+
+    it '#calorie_count' do
+      @chef_1 = Chef.create!(name: 'Guy Fieri')
+
+      @dish_1 = @chef_1.dishes.create!(name:"fish-eye pudding", description:"slimey pudding")
+      @dish_2 = @chef_1.dishes.create!(name:"stone soup", description:"a classic")
+
+      @ingredient_1 = @dish_1.ingredients.create!(name:"fish eyes", calories:1000)
+      @ingredient_2 = @dish_1.ingredients.create!(name:"vanilla pudding", calories:500)
+      @ingredient_3 = @dish_2.ingredients.create!(name:"a stone", calories:1)
+
+      expect(@dish_1.calorie_count).to eq(1500)
+      expect(@dish_2.calorie_count).to eq(1)
+    end
+
+  end
 end
