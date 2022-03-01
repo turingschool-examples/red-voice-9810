438e84b9cce13bfc50e80cd242e26024cfdec676
Sam Iavri <sammi80134@gmail.com>
2022-03-01 14:42:08 -0700

    Relationships between data bases set up

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Ingredient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should have_many :dish_ingredients }
+    it { should have_many(:dishes).through(:dish_ingredients) }
+  end
 end
