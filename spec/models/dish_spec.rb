0f67a2d98bbe4c8ff6d202cb3ce52acb40e5cb2f
Becky Nisttahuz <benistta@hotmail.com>
2022-03-01 14:48:45 -0700

    Test: method for total calorie count tested and show spec for dishes tests tested

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -44,5 +44,21 @@ RSpec.describe Dish, type: :model do
 
       expect(dish_1.name_of_chef).to eq("Gordon Ramsey")
     end
+
+    it 'stotal calorie count' do
+      chef = Chef.create(name: "Gordon Ramsey")
+      dish_1 = chef.dishes.create(name: "Roasted beef", description: "hot dish")
+      dish_2 = chef.dishes.create(name: "Potatoe Salad", description: "cold dish")
+
+      beef = Ingredient.create(name: 'Beef', calories: 300)
+      pepper = Ingredient.create(name: 'Pepper', calories: 30)
+      potatoes = Ingredient.create(name: 'Potatoes', calories: 170)
+
+      IngredientDish.create(dish_id: dish_1.id, ingredient_id: beef.id)
+      IngredientDish.create(dish_id: dish_1.id, ingredient_id: pepper.id)
+      IngredientDish.create(dish_id: dish_2.id, ingredient_id: potatoes.id)
+
+      expect(dish_1.total_calorie_count).to eq(330)
+    end
   end
 end
