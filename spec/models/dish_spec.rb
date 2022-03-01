9703e7cbaff4bdaf6203ec033bc62df3bdf7cb87
TJ Williams <williamsjr.timothy1@gmail.com>
2022-03-01 18:15:35 -0500

    Test/Feat: Dish can return #calorie_count

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,21 @@ RSpec.describe Dish, type: :model do
     it {should have_many(:ingredients).through(:dish_ingredients)}
     it {should belong_to :chef}
   end
+
+
+  before(:each) do
+    @chef = Chef.create!(name: "Mario")
+    @dish = @chef.dishes.create!(name: "Fettucine Alfredo", description: "Deliciousness")
+    @ingredient = Ingredient.create!(name: "Pasta", calories: 300)
+    @ingredient2 = Ingredient.create!(name: "Alfredo Sauce", calories: 200)
+    @ingredient3 = Ingredient.create!(name: "Pepper", calories: 10)
+    @dish_ingredient = DishIngredient.create!(dish_id: @dish.id, ingredient_id: @ingredient.id)
+    @dish_ingredient2 = DishIngredient.create!(dish_id: @dish.id, ingredient_id: @ingredient2.id)
+    @dish_ingredient3 = DishIngredient.create!(dish_id: @dish.id, ingredient_id: @ingredient3.id)
+  end
+
+  it ".calorie_count" do
+
+    expect(@dish.calorie_count).to eq(510)
+  end
 end
