3b4cb0ac670b8c2d38943240b6c7c62133855a61
Ruben Pasillas <hobbiathan@gmail.com>
2022-03-01 17:48:58 -0700

    ::get_ingredients for chef model, update views and controllers for chef and chef_ingredients

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,10 @@ class Chef <ApplicationRecord
   validates_presence_of :name
   has_many :dishes
 
+  def get_ingredients
+    dishes = self.dishes
+    ingredients = dishes.map do |dish|
+      dish.ingredients
+    end.uniq.flatten
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get '/dishes/:id', to: "dishes#show"
 
   get '/chefs/:id', to: "chefs#show"
+  get '/chefs/:id/ingredients', to: "chef_ingredients#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
