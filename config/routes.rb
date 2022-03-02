21b47452e04e1b3d741c29a0b8f06bf45c0af543
Ruben Pasillas <hobbiathan@gmail.com>
2022-03-01 17:29:56 -0700

    unfinished tests (i got distracted during my 'break')

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/dishes/:id', to: "dishes#show"
+
+  get '/chefs/:id', to: "chefs#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
